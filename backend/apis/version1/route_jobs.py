e8a44f2925113d19df55eaf5b61f48a16634b655
wai81 <50743966+wai81@users.noreply.github.com>
2022-01-09 19:47:27 +0300

    added get mehod for job and test methods

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 
 from db.session import get_db
 from db.models.jobs import Job
+from db.repository.jobs import retreive_job
 from schemas.jobs import JobCreate, ShowJob
 from db.repository.jobs import create_new_job
 
@@ -13,3 +14,13 @@ def create_job(job: JobCreate, db: Session = Depends(get_db)):
     owner_id = 1
     job = create_new_job(job=job, db=db, owner_id=owner_id)
     return job
+
+@router.get("/get/{id}", response_model=ShowJob)
+def retreive_job_by_id(id:int,db:Session=Depends(get_db)):
+    job = retreive_job(id=id, db=db)
+    if not job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                      detail=f'Job with ID:{id} does not exist')
+    return job
+        
+    
--- backend/tests/test_routes/test_jobs.py
+++ backend/tests/test_routes/test_jobs.py
@@ -13,3 +13,20 @@ def test_create_job(client):
     response = client.post("/jobs/create-job", json.dumps(data))
     
     assert response.status_code == 200
+
+
+def test_retreive_job_by_id(client):
+    data = {
+        "title": "SDE 1",
+        "company": "test company",
+        "company_url": "www.company.com",
+        "location": "UK, London",
+        "description": "Testing",
+        "date_posted": "2022-01-05"
+        }
+    
+    client.post("/jobs/create-job", json.dumps(data))
+    response = client.get("/job/get/1")
+    assert response.status_code == 200
+    assert response.json()["title"] == "SDE 1"
+    
