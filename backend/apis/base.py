71d22a7f018788b6cd1e23bc796e0b8411258a02
wai81 <50743966+wai81@users.noreply.github.com>
2022-01-09 20:09:01 +0300

    added method get_all_jobs

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -6,6 +6,6 @@ from apis.version1 import route_jobs
 
 api_router = APIRouter()
 
-api_router.include_router(route_users.router, prefix="/users", tags=["users"])
+api_router.include_router(route_users.router, prefix="/users", tags=["Users"])
 
-api_router.include_router(route_jobs.router, prefix="/jobs", tags=["jobs"])
+api_router.include_router(route_jobs.router, prefix="/jobs", tags=["Jobs"])
--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
@@ -5,7 +6,8 @@ from db.session import get_db
 from db.models.jobs import Job
 from db.repository.jobs import retreive_job
 from schemas.jobs import JobCreate, ShowJob
-from db.repository.jobs import create_new_job
+from db.repository.jobs import create_new_job, list_jobs
+from typing import List
 
 router = APIRouter()
 
@@ -22,5 +24,8 @@ def retreive_job_by_id(id:int,db:Session=Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                       detail=f'Job with ID:{id} does not exist')
     return job
-        
-    
+
+@router.get("/all", response_model=List[ShowJob])
+def retreive_all_jobs(db:Session=Depends(get_db)):
+    jobs = list_jobs(db=db)
+    return jobs
