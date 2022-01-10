e8d7b22fe13dfe47e1f8b9c6d6548e620f3b4b18
wai81 <50743966+wai81@users.noreply.github.com>
2022-01-10 23:30:21 +0300

    added JWT 1 pip install python-jose 2 ceate security.py 3 add to config.py SECRET_KEY 4 create login.py and add method get_user() 5 create route_login.pu and add login_for_access_token() 6 add route_login to base.py

---
+++ backend/apis/version1/route_login.py
@@ -0,0 +1,37 @@
+from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from datetime import timedelta
+
+from fastapi import APIRouter
+from db.session import get_db
+from core.config import settings
+from core.security import create_access_token
+from db.repository.login import get_user
+from core.hashing import Hasher
+
+router = APIRouter()
+
+def authenticate_user(username:str, password:str, db:Session):
+    user = get_user(username=username, db=db)
+    print(user)
+    if not user:
+        return False
+    if not Hasher.verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+
+
+@router.post("/token")
+def login_for_access_token(from_data:OAuth2PasswordRequestForm=Depends(), db:Session = Depends(get_db)):
+    user = authenticate_user( from_data.username, from_data.password,db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorect username or password")
+    
+    access_token_expire = timedelta(minutes = settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub":user.email}, expires_delta=access_token_expire)
+    return {"access_token":access_token, "token_type":"bearer"}
--- backend/core/config.py
+++ backend/core/config.py
@@ -7,14 +7,21 @@ load_dotenv(dotenv_path=env_path)
 
 
 class Setings:
+    # заголовок API
     PROJECT_TITLE:   str = "Jobboard"
     PROJECT_VERSION: str = "0.1.1"
     
+    # подключения к базе
     POSTGRES_USER:      str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD:  str = os.getenv("POSTGRES_PASSWORD")
     POSTGRES_SERVER:    str = os.getenv("POSTGRES_SERVER","localhost")
     POSTGRES_PORT:      str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
     POSTGRES_DB:        str = os.getenv("POSTGRES_DB","db_joddoard")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    
+    # настройки ключа JWT
+    SECRET_KEY:str = os.getenv("SECRET_KEY")
+    ALGORITHM ="HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
     
 settings = Setings()
