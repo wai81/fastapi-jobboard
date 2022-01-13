0ab0d5b636f65e553849d5219504d8beaf6e9498
wai81 <50743966+wai81@users.noreply.github.com>
2022-01-13 23:36:44 +0300

    added frontend template

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,36 @@
+from os import name
 from fastapi import FastAPI
 from core.config import settings
 from db.session import engine
 from db.base import Base
 from apis.base import api_router
-# from webapps.base import api_router as webapp_router
+from webapps.base import api_router as webapp_router
+from fastapi.staticfiles import StaticFiles
 
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
+
     
 def include_router(app):
-    app.include_router(api_router)
-#     app.include_router(webapp_router)
+    app.include_router(api_router)    # добавляем список маршрутов для API
+    app.include_router(webapp_router) # добавляем список маршрутов для web
+
+
+def configure_static(app):
+    app.mount("/staic", StaticFiles(directory="static"),name="static")
+
 
 def satrt_application():
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
-    create_tables()
-    include_router(app)
+    create_tables()     # создаем таблици при запуске приложения
+    include_router(app) # добавляем маршруты
+    configure_static(app) # создаем директорию для хранения статических файлов
     return app
+
 
 app = satrt_application()
 
-@app.get("/")
-def hello_api():
-    return {"detail":"Hello Wold!"}
+# @app.get("/")
+# def hello_api():
+#     return {"detail":"Hello Wold!"}
---
+++ backend/webapps/jobs/route_jobs.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi import Depends
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from db.repository.jobs import list_jobs
+from sqlalchemy.orm import Session
+from db.session import get_db
+from db.repository.jobs import retreive_job
+
+templates = Jinja2Templates(directory="templates")
+router = APIRouter(include_in_schema=False) # nclude_in_schema=False недобавляет в документацию API
+
+@router.get("/")
+def home(request:Request, db:Session=Depends(get_db), msg:str=None):
+    jobs = list_jobs(db=db)
+    return templates.TemplateResponse("jobs/homepage.html",
+                                      {"request":request,"jobs":jobs, "msg":msg})
+
+
+@router.get("/detail/{id}")
+def job_detail(id:int, request:Request, db:Session=Depends(get_db)):
+    job = retreive_job(id=id, db=db)
+    return templates.TemplateResponse("jobs/detail.html",
+                                      {"request":request,"job":job})
