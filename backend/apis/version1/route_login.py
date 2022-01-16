78d7fcffd7e16ad7869b325d6db42dd17be3fc3d
wai81 <50743966+wai81@users.noreply.github.com>
2022-01-16 21:15:37 +0300

    added authentication in web

--- backend/apis/version1/route_login.py
+++ backend/apis/version1/route_login.py
@@ -4,6 +4,7 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from jose import jwt, JWTError
+from fastapi import Response
 
 from fastapi import APIRouter
 from db.session import get_db
@@ -24,18 +25,19 @@ def authenticate_user(username:str, password:str, db:Session):
     return user
 
 
-@router.post("/token")
-def login_for_access_token(from_data:OAuth2PasswordRequestForm=Depends(), db:Session = Depends(get_db)):
-    user = authenticate_user( from_data.username, from_data.password,db)
+@router.post('/token')
+def login_for_access_token(response :Response,form_data:OAuth2PasswordRequestForm=Depends(),
+    db:Session = Depends(get_db)):
+    user = authenticate_user(form_data.username,form_data.password,db)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Incorect username or password")
-    access_token_expire = timedelta(minutes = settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub":user.email}, expires_delta=access_token_expire)
-    return {"access_token":access_token, "token_type":"bearer"}
+        detail="Incorrect username or password")
+    access_token_expire = timedelta(minutes= settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub":user.email},expires_delta=access_token_expire)
+    response.set_cookie(key="access_token",value=f"Bearer {access_token}",httponly=True)
+    return {"access_token":access_token,"token_type":"bearer"}
 
-
-oauth2_scheme =  OAuth2PasswordBearer(tokenUrl="/login/token")
+oauth2_scheme =  OAuth2PasswordBearer(tokenUrl='/login/token')
 
 
 def get_current_user_from_token(token:str = Depends(oauth2_scheme), db:Session=Depends(get_db)):
